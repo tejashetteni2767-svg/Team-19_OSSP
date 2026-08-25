b668735f57bfb3cb1029fc5d2b7fbd2ba7a1bbb3
tejashetteni2767-svg <tejashetteni2767@gmail.com>
2026-08-25 08:55:56 +0530

    Add files via upload

---
+++ skill/2520030419/Experiment1/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+int main() {
+    pid_t pid = fork();
+
+    if (pid < 0) {
+        printf("Fork failed!\n");
+    }
+    else if (pid == 0) {
+        printf("This is the Child Process.\n");
+        printf("Child PID = %d\n", getpid());
+        execlp("ls", "ls", "-l", NULL);
+    }
+    else {
+        printf("This is the Parent Process.\n");
+        printf("Parent PID = %d\n", getpid());
+        printf("Child PID = %d\n", pid);
+    }
+
+    return 0;
+}
